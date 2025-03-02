e690551bc36fb636e5c5a5c8287a7089cad99906
trinhquocnhat <tqnhat.24174600070@sv.uneti.edu.vn>
2025-03-02 20:52:46 +0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,6 @@
+n = float(input("nhap so nam:"))
+
+if (n % 4 == 0 and n % 100 != 0) or (n % 400 == 0):
+    print(f"{n} là năm nhuận.")
+else:
+    print(f"{n} không phải là năm nhuận.")
---
+++ 11.py
@@ -0,0 +1,5 @@
+n = int(input(" nhập số có 3 chữ số:"))
+if 100 <= n <= 999:
+    print(" số bạn nhập là số nguyên có 3 chữ số:",n)
+else:
+    print(" số nhập không phải là số nguyên có 3 chữ số")
