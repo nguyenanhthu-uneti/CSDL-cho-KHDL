02140dd6de28255f116bd419d4dc7644cb428c21
PhungQuocKhanh2808 <pqkhanh.24174600114@sv.uneti.edu.vn>
2025-03-02 20:30:33 +0700

    Add files via upload

---
+++ btap_tuan2.py
@@ -0,0 +1,11 @@
+# #Bai1
+# n = int(input("Nhap vao nam: "))
+# if n % 4 == 0 and n % 100 != 0:
+#     print("la nam nhuan")
+# elif n % 400 == 0:
+#     print("la nam nhuan")
+# else:
+#     print("khong la nam nhuan")
+
+
+#Bai2
